bdd4f628907e8348dfa5613adcea6b49e11e8f06
ChristopherFong604 <ccffoonngg@gmail.com>
2021-06-03 00:13:31 -0700

    Fixed negative weights and scores

--- src/main/resources/public/calculator.js
+++ src/main/resources/public/calculator.js
@@ -10,23 +10,23 @@ function addAssignment() {
     var table = document.getElementById("maintable");
     var row = table.insertRow(-1);
 
-    var 
-    Name = document.createElement('th'),
-    ShortName = document.createElement('th'),
-    Weight = document.createElement('th'),
-    Grade = document.createElement('th'),
-    Percentage = document.createElement('th')
-    IDtag = "A"+numAss;
-    
+    var
+        Name = document.createElement('th'),
+        ShortName = document.createElement('th'),
+        Weight = document.createElement('th'),
+        Grade = document.createElement('th'),
+        Percentage = document.createElement('th')
+    IDtag = "A" + numAss;
+
     Name.innerHTML = "Activity " + numAss;
-    ShortName.innerHTML = "A"+numAss;
+    ShortName.innerHTML = "A" + numAss;
     Weight.innerHTML =
-    '<input type="number" '+"name="+IDtag+"weight "+"id="+IDtag+"weight "+'value="0" onchange="percentCalculator(this.id)"/>';
-    Grade.innerHTML = 
-    '<input type="number" '+"name="+IDtag+"score "+"id="+IDtag+"score "+'value="0" onchange="percentCalculator(this.id)"/>'
-    +"/"+
-    '<input type="number" '+"name="+IDtag+"total "+"id="+IDtag+"total "+'value="0" onchange="percentCalculator(this.id)"/>';
-    Percentage.id = "P"+numAss;
+        '<input type="number" ' + "name=" + IDtag + "weight " + "id=" + IDtag + "weight " + 'value="0" onchange="percentCalculator(this.id)"/>';
+    Grade.innerHTML =
+        '<input type="number" ' + "name=" + IDtag + "score " + "id=" + IDtag + "score " + 'value="0" onchange="percentCalculator(this.id)"/>' +
+        "/" +
+        '<input type="number" ' + "name=" + IDtag + "total " + "id=" + IDtag + "total " + 'value="0" onchange="percentCalculator(this.id)"/>';
+    Percentage.id = "P" + numAss;
 
     row.appendChild(Name);
     row.appendChild(ShortName);
@@ -37,12 +37,12 @@ function addAssignment() {
 
 function removeAssignment() {
     numAss--;
-    if(numAss < 0) {
-        numAss=0;
+    if (numAss < 0) {
+        numAss = 0;
         return;
     }
     var table = document.getElementById("maintable");
-    table.deleteRow(-1);    
+    table.deleteRow(-1);
 }
 
 
@@ -50,30 +50,30 @@ function percentCalculator(id) {
     let score;
     let total;
     let percent;
-    weights[Number(id.charAt(1))] = document.getElementById("A"+id.charAt(1)+"weight").value;
-    switch(id.charAt(2)) {
+    weights[Number(id.charAt(1))] = document.getElementById("A" + id.charAt(1) + "weight").value;
+    switch (id.charAt(2)) {
         case "t":
-            score = Number(document.getElementById("A"+id.charAt(1)+"score").value);
+            score = Number(document.getElementById("A" + id.charAt(1) + "score").value);
             total = Number(document.getElementById(id).value);
-            if(total <= 0 || (score > total)) {
-                document.getElementById("P"+id.charAt(1)).innerHTML = "";
+            if (total <= 0 || score < 0 || (score > total)) {
+                document.getElementById("P" + id.charAt(1)).innerHTML = "";
                 break;
             }
-            percent = ((score/total)*100).toFixed(2);
+            percent = ((score / total) * 100).toFixed(2);
             percentages[Number(id.charAt(1))] = percent;
-            document.getElementById("P"+id.charAt(1)).innerHTML = percent + "%";
+            document.getElementById("P" + id.charAt(1)).innerHTML = percent + "%";
             break;
 
         case "s":
             score = Number(document.getElementById(id).value);
-            total = Number(document.getElementById("A"+id.charAt(1)+"total").value);
-            if(total <= 0 || (score > total)) {
-                document.getElementById("P"+id.charAt(1)).innerHTML = "";
+            total = Number(document.getElementById("A" + id.charAt(1) + "total").value);
+            if (total <= 0 || score < 0 || (score > total)) {
+                document.getElementById("P" + id.charAt(1)).innerHTML = "";
                 break;
             }
-            percent = ((score/total)*100).toFixed(2);
+            percent = ((score / total) * 100).toFixed(2);
             percentages[Number(id.charAt(1))] = percent;
-            document.getElementById("P"+id.charAt(1)).innerHTML = percent + "%";
+            document.getElementById("P" + id.charAt(1)).innerHTML = percent + "%";
             break;
     }
 }
@@ -81,40 +81,44 @@ function percentCalculator(id) {
 
 
 function getWeight() {
-    let i, rval = 0, 
-    actualAss = numAss,
-    totalweight = 0;
+    let i, rval = 0,
+        actualAss = numAss,
+        totalweight = 0;
 
-    for(i=1; i<=numAss; i++) {
-        if(percentages[i] === undefined) {
+    for (i = 1; i <= numAss; i++) {
+        if (percentages[i] === undefined) {
             actualAss--;
             continue;
         }
-        rval += Number(percentages[i])*Number(weights[i]);
+        if (weights[i] <= 0) {
+            document.getElementById("result").innerHTML = "Invalid weights inputted";
+        }
+        rval += Number(percentages[i]) * Number(weights[i]);
         totalweight += Number(weights[i]);
     }
-    let weightedmean = (rval/totalweight).toFixed(2);
-    if(isNaN(weightedmean) || actualAss <= 0) {
+    let weightedmean = (rval / totalweight).toFixed(2);
+    if (isNaN(weightedmean) || actualAss <= 0) {
         document.getElementById("result").innerHTML = "Invalid weights inputted";
         return;
     }
-    document.getElementById("result").innerHTML = "Result: " + weightedmean +"% is the weighted mean";
+    document.getElementById("result").innerHTML = "Result: " + weightedmean + "% is the weighted mean";
 }
 
 function getMean() {
-    let i, rval = 0, actualAss = numAss;
+    let i, rval = 0,
+        actualAss = numAss;
 
-    for(i=1; i<=numAss; i++) {
-        if(percentages[i] === undefined) {
+    for (i = 1; i <= numAss; i++) {
+        if (percentages[i] === undefined) {
             actualAss--;
             continue;
         }
         rval += Number(percentages[i]);
     }
-    let mean = (rval/actualAss).toFixed(2);
+    let mean = (rval / actualAss).toFixed(2);
     if (isNaN(mean) || actualAss <= 0) {
         document.getElementById("result").innerHTML = "Invalid inputs";
         return;
     }
-    document.getElementById("result").innerHTML = "Result: " + mean +"% is the mean";
+    document.getElementById("result").innerHTML = "Result: " + mean + "% is the mean";
 }
